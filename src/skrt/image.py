86809b9c32a3ca2b79dac6900c220fcfe13ba43b
K. Harrison <kh296@cam.ac.uk>
2021-11-23 16:25:17 +0000

    Updated check for null image source

--- src/skrt/image.py
+++ src/skrt/image.py
@@ -123,7 +123,7 @@ class Image(skrt.core.Archive):
         path = self.source if isinstance(self.source, str) else ""
         skrt.core.Archive.__init__(self, path)
 
-        if load or (isinstance(self.source, str) and not self.source):
+        if load and (not isinstance(self.source, str) or self.source):
             self.load_data()
 
     def get_data(self, standardise=False):
