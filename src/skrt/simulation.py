fixed dice calculation with mask for non-dicom-oriented arrays

src/skrt/simulation.py
@@ -100,13 +100,14 @@ class SyntheticImage(Image):
         """Update self.data so that it contains all current shapes."""
 
         # Get background array
-        data = self.get_background(force=force_bkg).copy()
+        data = self.get_background(with_noise=False).copy()
 
         # Add shapes
         for shape in self.shapes:
             data[shape.get_data(self.get_coords())] = shape.intensity
 
-        # Recreate image with current data
+        # Apply noise
+        data = self.apply_noise(data)
         self.data = data
 
         # Assign structure set
@@ -119,26 +120,26 @@ class SyntheticImage(Image):
         self.update()
         return Image(self)
 
-    def get_data(self):
+    def get_data(self, **kwargs):
         """Get Image data."""
 
         self.update()
-        return Image.get_data(self)
+        return Image.get_data(self, **kwargs)
 
     def plot(self, **kwargs):
         """Plot the current image with ROIs overlaid."""
 
         self.update()
         self.update_rois()
-        Image.plot(self, rois=-1, **kwargs)
+        kwargs.setdefault("rois", -1)
+        Image.plot(self, **kwargs)
 
     def view(self, **kwargs):
         """View the current image with QuickViewer."""
 
         from skrt.better_viewer import BetterViewer
-        return BetterViewer(self.get_image(), 
-                            rois=self.get_structure_set(), 
-                            **kwargs)
+        kwargs.setdefault("rois", self.get_structure_set())
+        return BetterViewer(self.get_image(), **kwargs)
 
     def get_roi_data(self):
         """Get dict of ROIs and names with any transformations applied."""
@@ -210,17 +211,20 @@ class SyntheticImage(Image):
         structure_set.write(outdir=outdir, ext=ext_to_use, 
                             overwrite=overwrite_roi_dir)
 
-    def get_background(self, force=False):
+    def get_background(self, with_noise=True, force=False):
         """Make blank image array or noisy array."""
 
-        if hasattr(self, "background") and not force:
-            return self.background
-
         bkg = np.ones(self.shape) * self.bg_intensity
-        if self.noise_std is not None:
-            bkg += np.random.normal(0, self.noise_std, self.shape)
-        self.background = bkg
+        if with_noise:
+            bkg = self.apply_noise(bkg)
         return bkg
+
+    def apply_noise(self, array):
+        """Apply background noise to an array."""
+
+        if self.noise_std is not None:
+            array += np.random.normal(0, self.noise_std, array.shape)
+        return array
 
     def set_noise_std(self, std):
 

src/skrt/structures.py
@@ -444,7 +444,7 @@ class ROI(skrt.core.Archive):
         self.load()
         self.create_mask()
         if not flatten:
-            return self.mask.get_data(standardise=False)
+            return self.mask.get_data(standardise=standardise)
         return np.sum(
             self.mask.get_data(standardise=standardise), 
             axis=skrt.image._slice_axes[view]
@@ -850,7 +850,7 @@ class ROI(skrt.core.Archive):
         pos=None,
         units="mm",
         method=None,
-        force=True
+        force=True,
     ):
         """Get either 3D global centroid position or 2D centroid position on 
         a single slice. If single_slice=False, the calculated global centroid 
@@ -1950,8 +1950,8 @@ class ROI(skrt.core.Archive):
 
             # Use 3D mask
             if not single_slice:
-                data1 = self.get_mask(view, flatten)
-                data2 = other.get_mask(view, flatten)
+                data1 = self.get_mask(view, flatten, standardise=True)
+                data2 = other.get_mask(view, flatten, standardise=True)
 
             # Otherwise, use single 2D slice
             else:
@@ -2586,6 +2586,7 @@ class ROI(skrt.core.Archive):
                 zoom_centre=zoom_centre,
                 show=False,
                 include_image=include_image,
+                color=color,
                 **kwargs,
             )
 
@@ -2760,7 +2761,6 @@ class ROI(skrt.core.Archive):
                         self.get_centre()[skrt.image._slice_axes[view]]
 
         kwargs["show"] = False
-        print(kwargs)
 
         # View with image
         if include_image and self.image is not None:
@@ -3259,7 +3259,8 @@ class StructureSet(skrt.core.Archive):
                     # Ignore entries containing only a single point
                     contours = roi["contours"]
                     if len(contours) == 1:
-                        if len(contours[list(contours.keys())[0]]) == 1:
+                        single_contour = contours[list(contours.keys())[0]][0]
+                        if single_contour.shape[0] == 1:
                             continue
 
                     self.rois.append(

tests/test_registration.py
@@ -35,7 +35,7 @@ def test_init_with_pfiles():
     """Test creation of a new Registration object with images and parameter
     files."""
 
-    pfiles =  ["pfiles/MI_Translation.txt", "pfiles/MSD_Rigid.txt"]
+    pfiles =  ["pfiles/MI_Translation.txt", "pfiles/MI_Affine.txt"]
     reg = Registration(reg_dir, im1, im2, pfiles=pfiles, overwrite=True)
     assert len(reg.steps) == len(pfiles)
     assert os.path.isfile(reg.steps_file)
@@ -116,7 +116,7 @@ def test_adjust_parameters():
 def test_run_registration():
     """Test running of a multi-step registration."""
 
-    pfiles = ["pfiles/MI_Translation.txt", "pfiles/MSD_Rigid.txt"]
+    pfiles = ["pfiles/MI_Translation.txt", "pfiles/MI_Affine.txt"]
     reg = Registration(
         reg_dir, 
         overwrite=True, 
@@ -149,14 +149,14 @@ def test_transform_image():
 
     reg = Registration(reg_dir)
     sim3 = SyntheticImage(sim2.shape)
-    sim4 = reg.transform_image(sim3)
+    sim4 = reg.transform(sim3)
     assert sim4.get_standardised_data().shape \
             != sim3.get_standardised_data().shape
     assert sim4.get_standardised_data().shape \
             == sim1.get_standardised_data().shape
 
     # Test transforming with an earlier step
-    sim5 = reg.transform_image(sim3, step=0)
+    sim5 = reg.transform(sim3, step=0)
     assert not(np.all(sim5.get_standardised_data() 
                       == sim4.get_standardised_data()))
     assert sim5.get_standardised_data().shape \
@@ -169,7 +169,17 @@ def test_transform_roi():
     sim3 = SyntheticImage(sim2.shape)
     sim3.add_cube(4, name="cube")
     roi = sim3.get_roi("cube")
-    roi2 = reg.transform_roi(roi)
-    assert roi.get_contours() != roi2.get_contours()
-    assert roi.get_mask().get_standardised_data().shape \
-            == sim1.get_standardised_data().shape
+    roi2 = reg.transform(roi)
+    assert roi2.get_contours() != roi.get_contours()
+    assert roi2.get_mask(standardise=True).shape \
+            == sim1.get_data(standardise=True).shape
+
+def test_transform_structure_set():
+
+    reg = Registration(reg_dir)
+    sim3 = SyntheticImage(sim2.shape)
+    sim3.add_cube(4, name="cube")
+    sim3.add_sphere(2, name="sphere")
+    ss = reg.transform(sim3.get_structure_set())
+    assert len(ss.rois) == len(sim3.get_structure_set().rois)
+