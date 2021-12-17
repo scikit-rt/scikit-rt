d40f1664787b2234c38f53081a483d28018bf91b
K. Harrison <kh296@cam.ac.uk>
2021-12-17 19:31:38 +0000

    Set python path explicitly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../src/skrt'))
+sys.path.insert(0, os.path.abspath('../src/skrt/better_viewer'))
+sys.path.insert(0, os.path.abspath('../src/skrt/viewer'))
 
+import skrt
 
 # -- Project information -----------------------------------------------------
 
