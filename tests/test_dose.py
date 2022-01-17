11b1fb3ac86041f60aaefcc8204cf9b775a5d255
K. Harrison <kh296@cam.ac.uk>
2022-01-17 13:50:28 +0000

    Added test for plot_DVH()

--- tests/test_dose.py
+++ tests/test_dose.py
@@ -6,6 +6,7 @@ import pytest
 import numpy as np
 
 from skrt import Dose, Image
+from skrt.simulation import SyntheticImage
 
 # Make random arrays to use for an image and a dose map
 im_array = np.random.rand(100, 150, 30)
@@ -15,6 +16,16 @@ dose_array = np.random.rand(100, 150, 30)
 tmp_path = Path('tmp')
 if not tmp_path.exists():
     tmp_path.mkdir()
+
+def get_synthetic_data():
+    # Create synthetic structure set
+    sim = SyntheticImage((100, 100, 40))
+    sim.add_cube(side_length=40, name="cube", intensity=1)
+    sim.add_sphere(radius=20, name="sphere", intensity=10)
+    structure_set = sim.get_structure_set()
+    # Create dose object from the synthetic image
+    dose = Dose(sim)
+    return (dose, structure_set)
 
 def test_create_dose_from_array():
     dose = Dose(dose_array)
@@ -83,3 +94,35 @@ def test_dcm_to_dcm():
     assert np.all(dose_dcm1.affine == dose_dcm2.affine)
     assert np.abs(dose_dcm1.get_data() - dose_dcm2.get_data()).max() < 0.005
     assert dose_dcm2.get_dicom_dataset().SeriesDescription == series_description
+
+def test_plot_dvh():
+    dose, structure_set = get_synthetic_data()
+    cube = structure_set.get_roi('cube')
+    dose_max = dose.get_max_dose_in_rois(structure_set.get_rois())
+    pdf = Path('tmp/dvh.pdf')
+    pdf.unlink(missing_ok=True)
+
+    # Check that pdf-file doesn't initially exist, but is created.
+    assert not pdf.exists()
+    ax = dose.plot_DVH(rois=[cube, structure_set], fname=str(pdf))
+    assert pdf.exists()
+
+    # Check data bounds
+    x1, x2 = ax.get_xbound()
+    y1, y2 = ax.get_ylim()
+    dx, dy = ax.margins()
+    x_range = (x2 - x1) / (1 + 2 * dx)
+    ddx = x_range * dx
+    assert x1 + ddx == 0
+    assert x2 - ddx == dose_max
+    assert y1 == 0
+    assert y2 - dy == 1
+
+    # Check that expected rois are included in the dose-volume histogram.
+    roi_names = structure_set.get_roi_names()
+    labels = [text.get_text() for text in ax.get_legend().texts]
+    assert len(roi_names) == len(labels)
+    while roi_names:
+        roi_name = roi_names.pop()
+        assert roi_name in labels
+        labels.remove(roi_name)
