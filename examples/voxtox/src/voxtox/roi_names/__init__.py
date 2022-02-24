0daebc7e1d04c9b97ccdaacb323826ccf01d3f90
K. Harrison <kh296@cam.ac.uk>
2022-02-24 19:48:03 +0000

    Added dictionaries for standardising ROI labels

---
+++ examples/voxtox/src/voxtox/roi_names/__init__.py
@@ -0,0 +1,9 @@
+'''Package providing dictionaries for standardising ROI labels.'''
+
+# Import dictionaries for head-and-neck cohort.
+from head_and_neck_roi_names import head_and_neck_plan,\
+            head_and_neck_voxtox, head_and_neck_iov, head_and_neck_mvct,\
+            head_and_neck_parotid_fiducials, head_and_neck_tre
+
+# Import dictionaries for prostate cohort.
+from prostate_roi_names import prostate_plan, prostate_voxtox, prostate_iov
