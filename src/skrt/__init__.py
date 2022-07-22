156761d3c8455004f7e9f61f83045a21d466ad56
K. Harrison <kh296@cam.ac.uk>
2022-07-23 00:31:45 +0100

    Suppress warning from pydicom

--- src/skrt/__init__.py
+++ src/skrt/__init__.py
@@ -1,4 +1,10 @@
+import warnings
+
 from skrt.image import Image
 from skrt.dose import Dose
 from skrt.structures import ROI, StructureSet
 from skrt.patient import Patient, Study
+
+# Suppress warning from pydicom.
+warnings.filterwarnings("ignore", message="Invalid value for VR UI",
+        module="pydicom")
