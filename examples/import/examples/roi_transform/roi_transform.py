ae7f3d0a0b5f35ba4e6ebeaca6a2d3dde6e2375a
K. Harrison <kh296@cam.ac.uk>
2022-09-08 10:30:13 +0100

    Specify memory required for running on Condor

--- examples/import/examples/roi_transform/roi_transform.py
+++ examples/import/examples/roi_transform/roi_transform.py
@@ -467,6 +467,7 @@ if 'Ganga' in __name__:
     # Define processing system.
     if "Linux" == platform.system():
         backend = Condor()
+        backend.cdf_options["request_memory"]="4G"
     else:
         backend = Local()
 
