fa94bd661cd9afe0c2a175faa3a3b0ea0bef9637
K. Harrison <kh296@cam.ac.uk>
2022-11-01 18:41:00 +0000

    Omit explict path settings for sphinx build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,12 +15,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
-sys.path.insert(0, os.path.abspath('../../src/skrt'))
-sys.path.insert(0, os.path.abspath('../../src/skrt/better_viewer'))
-sys.path.insert(0, os.path.abspath('../../src/skrt/viewer'))
-sys.path.insert(0, os.path.abspath('../../examples/voxtox/src'))
-sys.path.insert(0, os.path.abspath('../../examples/import_analysis/src'))
+# sys.path.insert(0, os.path.abspath('../../src'))
+# sys.path.insert(0, os.path.abspath('../../src/skrt'))
+# sys.path.insert(0, os.path.abspath('../../src/skrt/better_viewer'))
+# sys.path.insert(0, os.path.abspath('../../src/skrt/viewer'))
+# sys.path.insert(0, os.path.abspath('../../examples/voxtox/src'))
+# sys.path.insert(0, os.path.abspath('../../examples/import_analysis/src'))
 
 print(f'cwd: {os.getcwd()}')
 print(f'sys.path: {sys.path}')
