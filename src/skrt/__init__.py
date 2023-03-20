e9f131020ca45116fdb7208a81f60ba3265d255f
K. Harrison <kh296@cam.ac.uk>
2023-03-20 16:15:25 +0000

    Make more classes available from top-level package

--- src/skrt/__init__.py
+++ src/skrt/__init__.py
@@ -3,8 +3,11 @@ import warnings
 
 from skrt.better_viewer import BetterViewer
 from skrt.better_viewer.options import set_viewer_options
+from skrt.core import Defaults
 from skrt.image import Image
 from skrt.dose import Dose
+from skrt.registration import Registration
+from skrt.simulation import SyntheticImage
 from skrt.structures import ROI, StructureSet
 from skrt.patient import Patient, Study
 
