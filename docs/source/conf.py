e523ab4e016093298a6ed18752be1c53ff7ee1eb
K. Harrison <kh296@cam.ac.uk>
2023-04-17 11:38:05 +0100

    Updated version number: 0.4.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ copyright = '2023'
 #author = ''
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.1'
+release = '0.4.2'
 
 
 # -- General configuration ---------------------------------------------------
