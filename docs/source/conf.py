4c597813d0510ee450ff19e072e452cf518febeb
K. Harrison <kh296@users.noreply.github.com>
2023-07-16 17:25:41 +0100

    Updated version number: 0.5.13

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ copyright = '2023'
 #author = ''
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.12'
+release = '0.5.13'
 
 
 # -- General configuration ---------------------------------------------------
