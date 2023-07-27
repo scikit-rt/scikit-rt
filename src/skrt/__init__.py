930c0cedd2eda882da5efa88859af897ad7eb1a8
K. Harrison <kh296@users.noreply.github.com>
2023-07-27 17:19:19 +0100

    Formatting and linting for package module

--- src/skrt/__init__.py
+++ src/skrt/__init__.py
@@ -1,15 +1,28 @@
-import pkg_resources
+"""
+Toolkit for analysis of radiotherapy data.
+
+Scikit-rt, imported as skrt, is a toolkit for loading, plotting, and analysing
+radiotherapy data in DICOM and NIfTI formats.  It includes image registration
+via elastix, NiftyReg, and MATLAB, single- and multi-atlas segmentation,
+and region-of-interest (ROI) comparisons.
+
+Documentation: https://scikit-rt.github.io/scikit-rt/
+Code repository: https://github.com/scikit-rt/scikit-rt/
+PyPI project page: https://pypi.org/project/scikit-rt/
+"""
 import warnings
+
+import pkg_resources
 
 from skrt.better_viewer import BetterViewer
 from skrt.better_viewer.options import set_viewer_options
 from skrt.core import Defaults
-from skrt.image import Image
 from skrt.dose import Dose
+from skrt.image import Image
+from skrt.patient import Patient, Study
 from skrt.registration import Registration
 from skrt.simulation import SyntheticImage
 from skrt.structures import ROI, StructureSet
-from skrt.patient import Patient, Study
 
 # Suppress warning from pydicom.
 warnings.filterwarnings("ignore", message="Invalid value for VR UI",
