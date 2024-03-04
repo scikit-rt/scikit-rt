a5786588eefe858e35fe3089257749a543e9d733
K. Harrison <kh296@users.noreply.github.com>
2024-03-04 14:21:22 +0000

    Updated version number: 0.7.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ copyright = '2023'
 #author = ''
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.0'
+release = '0.7.1'
 
 
 # -- General configuration ---------------------------------------------------
