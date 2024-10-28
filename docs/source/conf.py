565795abb58e3965c6a9e18e24508ea8e9d20c7d
K. Harrison <kh296@users.noreply.github.com>
2024-10-28 18:18:34 +0000

    Updated version number: 0.8.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ copyright = '2023'
 #author = ''
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.0'
+release = '0.8.1'
 
 
 # -- General configuration ---------------------------------------------------
